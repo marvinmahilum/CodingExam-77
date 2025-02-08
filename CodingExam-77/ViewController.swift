adc4b7d11d05d28d972756ccb6ce8c1566675012
Marvin <marvin@Marvins-iMac.local>
2025-02-08 00:38:08 -0800

    Added .gitignore

---
+++ CodingExam-77/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingExam-77
+//
+//  Created by Marvin on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
